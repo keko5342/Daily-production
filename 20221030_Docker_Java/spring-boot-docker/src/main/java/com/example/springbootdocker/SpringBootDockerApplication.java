78d531dd55a61bdca5a1062f3b053495f2d008f9
keko <dn_ko_hei_21219@yahoo.co.jp>
2022-11-01 00:03:44 +0900

    vscode coding

--- 20221030_Docker_Java/spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ 20221030_Docker_Java/spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Docker World";
+    return "Hello Docker aaa";
   }
 
 	public static void main(String[] args) {
